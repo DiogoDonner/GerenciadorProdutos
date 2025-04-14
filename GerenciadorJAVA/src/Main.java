d7d387825f196f32d1e3577c2891d7f2a10ac4f0
DIOGO PATRICIO DONNER <diogo.donner@alunos.sc.senac.br>
2025-04-14 19:05:16 -0300

    Iniciação Projeto Sistema

---
+++ GerenciadorJAVA/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        boolean ativo = true;
+        while (ativo){
+
+            System.out.println("Escolha as opções\n" +
+                    "1 - Adicionar Item no Estoque\n");
+        }
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+    }
+}
