5071ef21162da875b07ec6d3b6d84d3a2090b1bf
DIOGO PATRICIO DONNER <diogo.donner@alunos.sc.senac.br>
2025-04-14 19:19:31 -0300

    Adicionada a função 1 e 2 ao sistema

--- GerenciadorJAVA/src/Main.java
+++ GerenciadorJAVA/src/Main.java
@@ -8,12 +8,35 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<String> Estoque = new ArrayList<>();
         boolean ativo = true;
-        while (ativo){
+        while (ativo) {
+            System.out.println("===Gerenciador de Estoque===");
+            Scanner scanner = new Scanner(System.in);
 
             System.out.println("Escolha as opções\n" +
-                    "1 - Adicionar Item no Estoque\n");
+                    "1 - Adicionar Item no Estoque\n" +
+                    "2 - Ver Lista \n" +
+                    "3 - Remover Item no Estoque\n" +
+                    "4 - Alterar Produto\n" +
+                    "5 - Sair");
+            String option = scanner.nextLine();
+            switch (option) {
+                case "1":
+                    System.out.println("===Gerenciador de Estoque===");
+                    System.out.println("Digite o nome do produto para adicionar à lista:");
+                    String add = scanner.nextLine();
+                    Estoque.add(add);
+                    System.out.println("O item " + add + "foi adicionado á lista");
+                    System.out.println(Estoque);
+                    break;
+                case "2":
+                    System.out.println("===Gerenciador de Estoque===");
+                    System.out.println("Estoque: " + Estoque);
+
+
+            }
+
         }
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
